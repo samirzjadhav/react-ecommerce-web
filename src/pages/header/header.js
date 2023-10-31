3cc5b09fd4b56d206553fe54070186898f149d6f
Samir Jadhav <samirzjadhav@gmail.com>
2023-10-31 22:16:27 +0530

    feat: added header for web

--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -2,9 +2,20 @@ import React from "react";
 
 const header = () => {
   return (
-    <div>
-      <h1>header</h1>
-    </div>
+    <section className="h-wrapper">
+      <div className="h-container">
+        <img src="./logo.png" alt="Logo" width={100} />
+        <div className="h-menu">
+          <a href="">Residencies</a>
+          <a href="">Our Values</a>
+          <a href="">Contact Us</a>
+          <a href="">Get Started</a>
+          <button className="button">
+            <a href="">Contact</a>
+          </button>
+        </div>
+      </div>
+    </section>
   );
 };
 
