f0b4bd2fd52d546bcdbbe658b31f552971ef85ea
Samir Jadhav <samirzjadhav@gmail.com>
2023-11-02 10:22:33 +0530

    feat: added styled on header

--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -5,12 +5,12 @@ const header = () => {
     <section className={styles.hWrapper}>
       <div className="flexCenter paddings innerwidth h-container">
         <img src="./logo.png" alt="Logo" width={100} />
-        <div className="h-menu">
+        <div className="flexCenter h-menu">
           <a href="">Residencies</a>
           <a href="">Our Values</a>
           <a href="">Contact Us</a>
           <a href="">Get Started</a>
-          <button className="button">
+          <button className="button header-btn">
             <a href="">Contact</a>
           </button>
         </div>
