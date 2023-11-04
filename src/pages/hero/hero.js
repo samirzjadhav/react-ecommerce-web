f9e0b8e577c89ae755aa39e4e15e476a3802d2c9
Samir Jadhav <samirzjadhav@gmail.com>
2023-11-04 10:14:34 +0530

    feat: added hero left section

--- src/pages/hero/hero.js
+++ src/pages/hero/hero.js
@@ -5,7 +5,21 @@ const hero = () => {
   return (
     <section className={styles.heroWrapper}>
       <div className="paddings innerWidth flexCenter hero-container">
-        <div className="hero-left">Left Section</div>
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <h1>
+              Discover <br />
+              Most Suitable <br /> Property
+            </h1>
+          </div>
+
+          <div className="hero-des flexColStart">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+
+          <div className="search-bar">search bar</div>
+        </div>
         <div className="flexCenter hero-right">
           <div className="image-container">
             <img src="./hero-image.png" alt="" />
