9a72d85d427aa49b0bcf6d4a17b215a4b8a46b82
Samir Jadhav <samirzjadhav@gmail.com>
2023-11-07 18:01:27 +0530

    feat: added styled in stats

--- src/pages/hero/hero.js
+++ src/pages/hero/hero.js
@@ -17,8 +17,12 @@ const hero = () => {
           </div>
 
           <div className="hero-des flexColStart">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">
+              Find a variety of properties that suit you very easilty
+            </span>
+            <span className="secondaryText">
+              Forget all difficulties in finding a residence for you
+            </span>
           </div>
 
           <div className="search-bar">
@@ -26,27 +30,27 @@ const hero = () => {
             <input type="text" />
             <button className="button">Search</button>
           </div>
-          <div className="flexCenter stats">
-            <div className="flexColStart stat">
+          <div className="flexCenter stats gap-[30px]">
+            <div className="flexColCenter stat">
               <span>
                 <CountUp start={8800} end={9000} duration={4} />
-                <span> +</span>
+                <span>+</span>
               </span>
-              <span>Premium Products </span>
+              <span className="secondaryText">Premium Products </span>
             </div>
-            <div className="flexColStart stat">
+            <div className="flexColCenter stat">
               <span>
                 <CountUp start={1950} end={2000} duration={4} />
-                <span> +</span>
+                <span>+</span>
               </span>
-              <span>Happy Customers </span>
+              <span className="secondaryText">Happy Customers </span>
             </div>
-            <div className="flexColStart stat">
+            <div className="flexColCenter stat">
               <span>
                 <CountUp end={28} />
-                <span> +</span>
+                <span>+</span>
               </span>
-              <span>Award Winning </span>
+              <span className="secondaryText">Award Winning </span>
             </div>
           </div>
         </div>
