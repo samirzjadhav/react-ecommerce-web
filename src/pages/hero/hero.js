c45488c9557a00da54c65e977804fa97d84ae6e2
Samir Jadhav <samirzjadhav@gmail.com>
2023-11-07 17:11:39 +0530

    feat: added counting in counting section

--- src/pages/hero/hero.js
+++ src/pages/hero/hero.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./style.module.css";
 import { HiLocationMarker } from "react-icons/hi";
+import CountUp from "react-countup";
 
 const hero = () => {
   return (
@@ -24,6 +25,29 @@ const hero = () => {
             <HiLocationMarker color="var(--blue)" size={25} />
             <input type="text" />
             <button className="button">Search</button>
+          </div>
+          <div className="flexCenter stats">
+            <div className="flexColStart stat">
+              <span>
+                <CountUp start={8800} end={9000} duration={4} />
+                <span> +</span>
+              </span>
+              <span>Premium Products </span>
+            </div>
+            <div className="flexColStart stat">
+              <span>
+                <CountUp start={1950} end={2000} duration={4} />
+                <span> +</span>
+              </span>
+              <span>Happy Customers </span>
+            </div>
+            <div className="flexColStart stat">
+              <span>
+                <CountUp end={28} />
+                <span> +</span>
+              </span>
+              <span>Award Winning </span>
+            </div>
           </div>
         </div>
         <div className="flexCenter hero-right">
