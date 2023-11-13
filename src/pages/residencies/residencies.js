207826df7b360e5d056f40e98b7d4f814b91deff
Samir Jadhav <samirzjadhav@gmail.com>
2023-11-13 22:22:58 +0530

    feat: added swiper for card

--- src/pages/residencies/residencies.js
+++ src/pages/residencies/residencies.js
@@ -8,9 +8,25 @@ const residencies = () => {
     <section className="r-wrapper">
       <div className="padding innerWidth r-container">
         <div className="r-head flexColStart">
-          <span>Best Choices</span>
-          <span>Popular residencies</span>
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Popular residencies</span>
         </div>
+
+        <Swiper>
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="flexColStart r-card">
+                <img src={card.image} alt="home" />
+                <span className="secondaryText r-price">
+                  <span style={{ color: "orange" }}>$</span>
+                  <span>{card.price}</span>
+                </span>
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
