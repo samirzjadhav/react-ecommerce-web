fef12c396f3c99ea9e8e5eae9bf87ef88c0c1ab0
Samir Jadhav <samirzjadhav@gmail.com>
2023-11-19 22:23:14 +0530

    feat: added contact modes

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { MdCall } from "react-icons/md";
+import { BsFillChatDotsFill } from "react-icons/bs";
+import { HiChatBubbleBottomCenter } from "react-icons/hi2";
 
 const contact = () => {
   return (
@@ -12,6 +15,24 @@ const contact = () => {
             We always ready to help by providijng the best services for you. We
             beleive a good blace to live can make your life better
           </span>
+
+          <div className="flexColStart contactModes">
+            {/* First Row */}
+            <div className="flexColStart row">
+              <div className="flexColCenter mode">
+                <div className="flexStart">
+                  <div className="flexCenter icon">
+                    <MdCall size={25} />
+                  </div>
+                  <div className="flexColStart detail">
+                    <span className="primaryText">call</span>
+                    <span className="secondaryText">021 123 145 14</span>
+                  </div>
+                </div>
+                <div className="flexCenter button">call now</div>
+              </div>
+            </div>
+          </div>
         </div>
         {/* right Side */}
         <div className="c-right">
