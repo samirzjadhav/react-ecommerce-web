bf5afa39de8d43957036f29e7f8611343ebe460a
Samir Jadhav <samirzjadhav@gmail.com>
2023-11-30 19:01:19 +0530

    feat: added sricpt for navbar

--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./styles.module.css";
 import { BiMenuAltRight } from "react-icons/bi";
-const header = () => {
+
+const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  const document = typeof window;
+  const getMenuStyle = (menuOpened) => {
+    if (document.documentElement.clientWidth <= 800) {
+      return { right: !menuOpened && "-100%" };
+    }
+  };
   return (
     <section className={styles.hWrapper}>
       <div className="flexCenter paddings innerwidth h-container">
         <img src="./logo.png" alt="Logo" width={100} />
-        <div className="flexCenter h-menu">
+        <div className="flexCenter h-menu" style={getMenuStyle(menuOpened)}>
           <a href="">Residencies</a>
           <a href="">Our Values</a>
           <a href="">Contact Us</a>
@@ -16,11 +24,14 @@ const header = () => {
           </button>
         </div>
       </div>
-      <div className="menu-icon hidden">
+      <div
+        className="menu-icon hidden"
+        onClick={() => setMenuOpened((prev) => !prev)}
+      >
         <BiMenuAltRight size={30} />
       </div>
     </section>
   );
 };
 
-export default header;
+export default Header;
