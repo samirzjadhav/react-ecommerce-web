579f26fd2a16a6d8c87fc18b7298921d7c8e10cf
Samir Jadhav <samirzjadhav@gmail.com>
2023-12-03 01:04:28 +0530

    feat: added padding in companies logos

--- src/pages/companies/companies.js
+++ src/pages/companies/companies.js
@@ -3,7 +3,7 @@ import React from "react";
 const companies = () => {
   return (
     <section className="c-wrappper">
-      <div className="padding innerWidth flexCenter c-container">
+      <div className="padding innerWidth flexCenter c-container px-[30px]">
         <img src="./prologis.png" alt="" />
         <img src="./tower.png" alt="" />
         <img src="./equinix.png" alt="" />
