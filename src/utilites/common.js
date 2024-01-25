0e49e0b144b9c7e8953774a1ed0ddf8728257619
Samir Jadhav <samirzjadhav@gmail.com>
2024-01-25 21:14:13 +0530

    fix: change sliderPerView to sliderperview

--- src/utilites/common.js
+++ src/utilites/common.js
@@ -3,16 +3,16 @@ export const sliderSettings = {
   spaceBetween: 50,
   breakpoints: {
     480: {
-      slidesPerView: 1,
+      slidesperview: 1,
     },
     600: {
-      slidesPerView: 2,
+      slidesperview: 2,
     },
     750: {
-      slidesPerView: 3,
+      slidesperview: 3,
     },
     1100: {
-      slidesPerView: 4,
+      slidesperview: 4,
     },
   },
 };
